df62d9b2bf26cbde6e0d8f82277e8127d228eb03
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-13 11:22:25 -0800

    configuring for Heroku

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const server = require('./server.js');
 
-server.listen(5000, console.log('* Server Running on http://localhost:5000 *'));
+const port = process.env.PORT || 5000;
+server.listen(port, console.log('* Server Running on http://localhost:5000 *'));
